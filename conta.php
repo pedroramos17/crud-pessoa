5f2ea0027c093950f064506b34b96908c84e850e
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-09-04 21:33:00 -0300

    wip

--- conta.php
+++ conta.php
@@ -1,5 +1,14 @@
 <?php
-
+require_once "pessoa.php";
 class Conta {
-    
+
+    new Pessoa();
+
+    int $agencia;
+    string $codigo;
+    $dataDeCriacao;
+    $titular;
+    string $senha;
+    float $saldo;
+    boolean $cancelada;
 }
