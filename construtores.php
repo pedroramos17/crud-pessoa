fe7026c9c708f7d7e6c59c51fd0d70bbce970f9f
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-09-06 21:49:35 -0300

    alter echos, add dot in the end

--- construtores.php
+++ construtores.php
@@ -5,21 +5,21 @@ include_once 'Conta.php';
 
 $carlos = new Pessoa(10, "Carlos de Almeida", 1.85, 25, "12837921361", "Ensino Médio", 650.00);
 
-echo "Manipulando o objeto {$carlos->nome} <br>";
+echo "Manipulando o objeto {$carlos->nome}.<br>";
 
 echo "<br>";
 
-echo "{$carlos->nome} é formado em: {$carlos->escolaridade} <br>";
+echo "{$carlos->nome} é formado em: {$carlos->escolaridade}.<br>";
 
-$carlos->formar("Técnico em Desenvolvimento de Sistemas");
+$carlos->formar("Técnico em Desenvolvimento de Sistemas.");
 
-echo "Carlos é formado agora {$carlos->escolaridade} <br>";
+echo "Carlos é formado agora {$carlos->escolaridade}.<br>";
 
-echo "A idade de Carlos é {$carlos->idade} <br>";
+echo "A idade de Carlos é {$carlos->idade}.<br>";
 
 $carlos->envelhecer(1);
 
-echo "Carlos agora tem {$carlos->idade} de idade <br>";
+echo "Carlos agora tem {$carlos->idade} de idade.<br>";
 
 $carlos_conta = new Conta(6671, "CC:11231243" , "2384092", $carlos, 1233, 23);
 
@@ -27,13 +27,13 @@ echo "/n";
 
 echo "Manipulando a conta de {$carlos_conta->titular->nome}. /n";
 
-echo "O saldo atual é {$carlos_conta->obterSaldo()} /n";
+echo "O saldo atual é {$carlos_conta->obterSaldo()}. /n";
 
 $carlos_conta->depositar(20);
 
-echo "O saldo agora é {$carlos_conta->obterSaldo()} /n";
+echo "O saldo agora é {$carlos_conta->obterSaldo()}. /n";
 
 $carlos_conta->retirar(10);
 
-echo "O novo saldo é {$carlos_conta->obterSaldo()}";
+echo "O novo saldo é {$carlos_conta->obterSaldo()}.";
 
